9210fc88697a4f464ac1abfbf54433c8e0409755
Kevin Smith <keveightysev@gmail.com>
2019-01-29 14:36:04 -0600

    Defined JS selectors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+const container = document.getElementsByClassName('container');
+const navLinks = Array.from(document.getElementsByClassName('nav-link'));
+const funBus = document.querySelector('.intro img');
+const midImgs = document.querySelectorAll('.img-content img');
+const destImg = document.querySelector('.content-destination img')
+const buttons = document.getElementsByClassName('btn');
+const logo = document.querySelector('.logo-heading');
+const nav = document.querySelector('.main-navigation');
